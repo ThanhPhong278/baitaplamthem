28e95abbcef93d572071bd83152615fb418c513e
nhutlinh1304 <nguyennhutlinh1304@gmail.com>
2019-10-31 17:37:16 +0700

    OOP làm thêm

---
+++ ConsoleApplication1/ConsoleApplication2/Author.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+   public class Author
+    {
+       private string Name;
+       private string Email;
+       private char Gender;
+
+       public Author (string name, string email, char gender)
+       {
+           this.Email = email;
+           this.Name = name;
+           this.Gender = gender;
+       }
+       public string GetName()
+       {
+           return this.Name;
+       }
+       public string GetEmail()
+       {
+           return this.Email;
+       }
+       public char GetGender()
+       {
+           return this.Gender;
+       }
+       public void SetEmail(string email)
+       {
+           this.Email = email;
+       }
+       public override string ToString()
+       {
+           return string.Format("Auto[name={0},email={1},gender={2}]", this.Name, this.Email, this.Gender);
+       } 
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication2/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Book
+    {
+        private string Name;
+        private Author[] Authors;
+        private Double Price;
+        private int Qty;
+
+        public Book(string name, Author[] authors, Double price )
+        {
+            this.Name = name;
+            this.Authors = authors;
+            this.Price = price;
+            this.Qty = 0;
+        }
+        public Book(string name, Author[] authors, Double price, int qty)
+            :this(name,authors,price)
+        {
+            this.Qty = qty;
+        }
+        public string GetName() { return this.Name; }
+        public Author[] GetAuthor() { return this.Authors; }
+        public Double GetPrice() { return this.Price; }
+        public int GetQty() { return this.Qty; }
+        public void SetPrice(Double price) { this.Price = price; }
+        public void SetQty(int qty) { this.Qty = qty; }
+        public override string ToString()
+        {
+            string authors = "";
+            for (int i = 0; i < this.Authors.Length; i++)
+                authors += this.Authors[i].ToString() + " , ";
+                return string.Format("Book [Name={0},{3}, authors={{{3}}},price={1},Qty={2}]",
+                    this.Name, this.Price, this.Qty, authors);
+            
+        }
+    }
+}
